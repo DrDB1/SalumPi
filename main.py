2cd6ad9fdbbe82aaedc8a6beabb1f58f01239292
DrDB1 <dominic.barone@gmail.com>
2019-01-27 23:09:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,27 +9,43 @@ GPIO.setmode(GPIO.BCM)
 humidifier = 18
 heater = 23
 freezer = 24
+fan = 0
 ambient_sensor = 20
 chamber_sensor = 21
 
+humidifier_status = 0
+heat_status = 0
+cool_status = 0
+fan_status = 0
 
+# Setup GPIO Pins
 GPIO.setup(humidifier, GPIO.OUT)
 GPIO.setup(heater, GPIO.OUT)
 GPIO.setup(freezer, GPIO.OUT)
 
-chamber_sen = Adafruit_DHT.DHT22
-ambient_sen = Adafruit_DHT.DHT22
-
-RUN = true
-
-
 # Get Sensor Readings
-amb_relhum, amb_temp = Adafruit_DHT.read_retry(ambient_sensor, ambient_pin)
-cbm_relhum, cbm_temp = Adafruit_DHT.read_retry(chamber_sensor, chamber_pin)
+amb_relhum, amb_temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, ambient_sensor)
+cbm_relhum, cbm_temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, chamber_sensor)
+
+thingspeak_key = 'H3HJYAGBW2M3DDEX'
+
+r = requests.post('https://api.thingspeak.com/update.json', 
+	data = {'api_key':thingspeak_key, 
+	'field1':cbm_temp, 
+	'field2':cbm_relhum,
+	'field3':amb_temp
+	'field4':amb_relhum
+	'field5':cool_status
+	'field6':heat_status
+	'field7':humidifier_status
+	'field8':fan_status
+	})
+
 
 # Print Sensor Readings
-print('              Temp    RelHum\n'
-print('           --------  --------\n'
-print(' Ambient    %4.1f     %4.2f' % (amb_temp, amb_relhum))
-print(' Chamber    %4.1f     %4.2f' % (cbm_temp, cbm_relhum))
+print('              Temp    RelHum')
+print('           --------  --------')
+print('  Ambient     %4.1f     %4.2f' % (amb_temp, amb_relhum))
+print('  Chamber     %4.1f     %4.2f' % (cbm_temp, cbm_relhum))
 	
+GPIO.cleanup()
