74f97c3140e8fb0ef14be6cf43722f88b879475a
DrDB1 <dominic.barone@gmail.com>
2019-02-09 20:37:24 -0500

    Create salumipi.py

---
+++ salumipi.py
@@ -0,0 +1,48 @@
+import Adafruit_DHT
+import time
+
+class TH_Sensor:
+	def __init__(self, pin=None):
+		units = 'C'
+		
+		self.pin = pin # 
+		self.T  = 0. # [C] Reporting Temperature
+		self.Tk = 0. # Temperature in Kelvin
+		self.RH = 0. # Relative Humidity
+		self.p_air = 101325. # [Pa] Air Pressure 
+		self.rho_air = 0. # [kg/m3] Density of Air
+		self.pp_h20 = 0. # Partial Pressure of Water 
+		
+		self.last_reading = 0.
+		self.TMIN = -20. # [C] Minimum valid temperature 
+		self.TMAX = 50.  # [C] Maximum valid temperature
+		self.RHMIN = 0. # [C] Minimum relative humidity 
+		self.RHMAX = 110.  # [C] Maximum relative humidity
+		self.NMAX = 10 # Max number of reading attempts'
+		self.VERBOSE = 0;
+		
+	def get_data(self):
+		VALID = False
+		n = 0
+		while not VALID and (n<self.NMAX)
+			RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, self.pin)
+			if VERBOSE: print('%.1f C, %.1%%RH' % (T, RH))
+			
+			# test for valid temp RH ranges
+			if RH <= self.RHNAX and RH >= self.RHMIN and T >= self.TMIN and T =< self.TMAX
+				VALID = True
+				self.T = T
+				self.RH = RH
+				self.Tk = T+273.15
+				if VERBOSE: print('Valid Reading')
+			else:
+				if VERBOSE: print('Invalid Reading, Loop %.0f' % n)
+			n+=1
+			
+			
+if __name__ == '__main__':
+	test = TH_Sensor(20)
+	test.VERBOSE = True
+	test.get_data()
+	print(test.T)
+	print(test.RH)
