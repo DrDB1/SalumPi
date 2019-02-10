6ce1cfc6d34a4b1ed5ea00e0deabfa21528a183d
DrDB1 <dominic.barone@gmail.com>
2019-02-10 15:39:50 -0500

    Update salumipi.py

--- salumipi.py
+++ salumipi.py
@@ -1,9 +1,52 @@
 import Adafruit_DHT
 import time
 import numpy as np
+import RPi.GPIO as GPIO
 
 
-
+class Chamber:
+	def __init__(self):
+		self.R = 287.058 # J/kg-K
+		self.cp_air = 1.005 # kJ/kg-K
+		self.cp_h20 = 1.996 # kJ/kg-K
+		# Chamber Properties
+		
+		self.V = 0.4 # m3 Chamber Volume
+		self.A = 3.6 # m2 Chamber Area
+		self.t = .05 # m  Chamber insulation thickness
+		self.k = 0.026 # [W/m-K] Chamber insulation conductivity
+		
+		# Air Properties
+		self.T = None # Temperature in K
+		self.RH = None # Relative Humidity
+		self.p = 101325. # [Pa] Standard Pressure
+		self.Q = None # [kJ] Thermal Energy 
+		
+		self.rho_air = 0. # [kg/m3] Density of Air
+		self.rho_h20 = 0. # Water vapor density
+		
+		self.p_sat = 0. # Saturation Pressure of Water
+		self.rho_sat = 0. # Saturation Water vapor density
+		self.m_air = 0. # mass of air
+		self.m_h20 = 0. # mass of water
+	
+	def update(T,RH)
+		# Air Conditions
+		self.T = T+273.15
+		self.rho_air = self.p/(self.R*self.T)
+		
+		# Water Conditions
+		self.RH = RH
+		self.p_sat = np.exp(77.3450 + 0.0057*T - 7235/T) / (T**8.2) 
+		self.rho_sat = 0.0022 * self.p_sat / T
+		self.rho_h20 = self.RH*self.rho_sat
+		
+		self.m_air = self.rho_air*self.V
+		self.m_h20 = self.rho_h20*self.V
+		
+		# Energy
+		self.Q = self.cp_air * self.m_air * self.T + self.cp_h20 * self.m_h20 * self.T
+		
 class Heater:
 	def __init__(self, pin=None):
 		self.pin = pin 
@@ -13,18 +56,8 @@ class Heater:
 class TH_Sensor:
 	def __init__(self, pin=None):		
 		self.pin = pin # 
-		self.T  = 0. # [C] Reporting Temperature
-		self.Tk = 0. # Temperature in Kelvin
+		self.T  = 0. # [C] Reporting Temperature		
 		self.RH = 0. # Relative Humidity
-		self.p_air = 101325. # [Pa] Air Pressure 
-		self.rho_air = 0. # [kg/m3] Density of Air
-		
-		
-		self.p_h20 = 0. # Partial Pressure of Water 
-		self.rho_h20 = 0. # Water vapor density
-		
-		self.p_sat = 0. # Saturation Pressure of Water
-		self.rho_sat = 0. # Saturation Water vapor density
 		
 		self.last_reading = 0.
 		self.TMIN = -20. # [C] Minimum valid temperature 
@@ -45,20 +78,12 @@ class TH_Sensor:
 			if RH <= self.RHMAX and RH >= self.RHMIN and T >= self.TMIN and T <= self.TMAX:
 				VALID = True
 				self.T = T
-				self.RH = RH
-				self.Tk = T+273.15
+				self.RH = RH				
 				self.last_reading = time.time()
 				if self.VERBOSE: print('Valid Reading')
 			else:
 				if self.VERBOSE: print('Invalid Reading, Loop %.0f' % n)
 			n+=1
-			
-	def calc_saturation(self):
-		T = self.Tk
-		# estimate saturation pressure
-		# https://www.engineeringtoolbox.com/water-vapor-saturation-pressure-air-d_689.html
-		self.p_sat = np.exp(77.3450 + 0.0057*T - 7235/T) / (T**8.2) 
-		self.rho_sat = 0.0022 * self.p_sat / T 
 			
 if __name__ == '__main__':
 	test = TH_Sensor(20)
